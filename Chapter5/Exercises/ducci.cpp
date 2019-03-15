2a28089f5ae6524130c75af191864d80df9f360e
AlcEccentric <lihongyide@gmail.com>
2019-03-15 09:48:19 +0800

    chap5-exer-ducci AC

--- Chapter5/Exercises/ducci.cpp
+++ Chapter5/Exercises/ducci.cpp
@@ -3,27 +3,57 @@
 #include <cstring>
 #include <stdlib.h>
 #include <vector>
+#include <set>
 using namespace std;
-ifstream fin("test.in");
-ofstream fout("test.out");
+// ifstream  cin("test.in");
+// ofstream  cout("test.out");
 
 int main(){
     int testCaseNum;
-    fin >> testCaseNum;
+     cin >> testCaseNum;
     for(int i = 0; i < testCaseNum; i++)
     {
         int n;
-        fin >> n;
+         cin >> n;
         vector<int> nums;
         for(int j = 0; j < n; j++)
         {
             int num;
-            fin >> num;
-            fout << num << " ";
+             cin >> num;
+            //  cout << num << " ";
             nums.push_back(num);
-        }fout << endl;
-        
-        
+        }
+        //  cout << endl;
+
+        bool allZero = false;
+        for(int i = 0; i < 1000; i++)
+        {
+            int firstNum = nums[0];
+            set<int> remains;
+            for(vector<int>::iterator ii = nums.begin(); ii != nums.end(); ii++)
+            {   
+
+                if(ii != nums.end() - 1){
+                    int diff = abs(*(ii + 1) - *(ii));
+                    nums[ii - nums.begin()] = diff;
+                    remains.insert(diff);
+                }
+                else{
+                    int diff = abs(firstNum - *(ii));
+                    nums[ii - nums.begin()] = diff;
+                    remains.insert(diff);
+                }
+
+            }
+            if(remains.size() == 1 && *remains.begin() == 0){
+                allZero = true;
+                break;
+            }
+        }
+        if(allZero)
+             cout << "ZERO" << endl;
+        else
+             cout << "LOOP" << endl;
     }
     
 }
